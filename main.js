0a882e4926c9ffe511d8387a041cf31dc7bba9fe
Rexolt <157226862+Rexolt@users.noreply.github.com>
2025-01-31 13:56:08 +0100

    FIX: electron window controls

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const { ipcMain } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    frame: true, 
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true
+      preload: path.join(__dirname, "preload.js"), 
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
 
-  win.loadFile(path.join(__dirname, 'web', 'viewer.html'));
+  win.loadFile(path.join(__dirname, "web", "viewer.html"));
 }
 
 app.whenReady().then(() => {
   createWindow();
+});
+
+
+ipcMain.on("minimize-window", (event) => {
+  BrowserWindow.getFocusedWindow().minimize();
+});
+
+ipcMain.on("maximize-window", (event) => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win.isMaximized()) {
+    win.unmaximize();
+  } else {
+    win.maximize();
+  }
+});
+
+ipcMain.on("close-window", (event) => {
+  BrowserWindow.getFocusedWindow().close();
 });
